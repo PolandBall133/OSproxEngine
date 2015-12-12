285ca7d45be80d74fe128619788eff897cab4e1d
Patryk Wertka <spartanpageagain@gmail.com>
2015-12-12 20:14:59 +0100

    Added shader program pseudo-module organizer

---
+++ include/core/shaders/shader.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace OSprox{
+namespace Shaders{
+
+//<OSprox::Shaders::ShaderProgramID>
+#include "shaders_/shader_identification.hpp"
+//</OSprox::Shaders::ShaderProgramID>
+
+//<OSprox::Shaders::ShaderProgram>
+#include "shaders_/shader_program.hpp"
+//</OSprox::Shaders::ShaderProgram>
+}}
